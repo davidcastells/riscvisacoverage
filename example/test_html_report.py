af190466749a75c55bb4d0db7f2efaa3c55d404c
David Castells-Rufas <david.castells@uab.cat>
2022-07-19 17:58:17 +0200

    relative

--- example/test_html_report.py
+++ example/test_html_report.py
@@ -11,7 +11,7 @@ sys.path.append('..')
 
 import riscisacoverage as isacov
 
-isacov.create_html_report('test.riscv-32.exe', 'test.riscv-32.html')
-isacov.create_html_report('test_nostd.riscv-32.exe', 'test_nostd.riscv-32.html')
+isacov.create_html_report('test.riscv-32.exe', '../docs/test.riscv-32.html')
+isacov.create_html_report('test_nostd.riscv-32.exe', '../docs/test_nostd.riscv-32.html')
 
 print('done!')
